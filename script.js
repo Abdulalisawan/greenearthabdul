85e628b467f36e616ad310cb7530765a2f3430ec
Abdulalisawan <abdulalisawan.dev@gmail.com>
2025-09-09 13:06:43 +0600

    error fixed

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 // categories
 
+
+const spinner=(status)=>{
+    if(status == true){
+        let spinnermotion=document.querySelector(`#spinning`).classList.remove(`hidden`);
+        let spinnermo=document.querySelector(`#spinn`).classList.remove(`hidden`);
+        let alltreecontent=document.querySelector(`#alltreecontent`).classList.add(`hidden`);
+    }else{
+        let spinnermotion=document.querySelector(`#spinning`).classList.add(`hidden`);
+        let spinnermo=document.querySelector(`#spinn`).classList.add(`hidden`);
+        let alltreecontent=document.querySelector(`#alltreecontent`).classList.remove(`hidden`);
+
+        
+    }
+}
+
+
+
 const cetagories = ()=>{
+    
     fetch(`https://openapi.programming-hero.com/api/categories`)
     .then(res=>res.json())
     .then(values=>displayCetagories(values.categories))
+    .catch(error => console.error('Error:', error)) 
 }
 
 const displayCetagories=(categoryvalue)=>{
@@ -13,38 +32,30 @@ const displayCetagories=(categoryvalue)=>{
         div1.innerHTML=`<div id="coloranimate${eachvalue.id}" onclick="subcetagory(${eachvalue.id})" class="mt-3 primarybtncolor text-xl hover:bg-[#a6beaf] py-2 rounded-3xl pl-4"><h1>${eachvalue.category_name}</h1></div>`;
         catagorycontent.appendChild(div1)
     }
-
+      
 }
 cetagories();
-const spinner=(status)=>{
-    if(status == true){
-        let spinnermotion=document.querySelector(`#spinning`).classList.remove(`hidden`);
-        let alltreecontent=document.querySelector(`#alltreecontent`).classList.add(`hidden`);
-    }else{
-        let spinnermotion=document.querySelector(`#spinning`).classList.add(`hidden`);
-        let alltreecontent=document.querySelector(`#alltreecontent`).classList.remove(`hidden`);
 
-        
-    }
-}
 
 const alltree =()=>{
     spinner(true);
     fetch(`https://openapi.programming-hero.com/api/plants`)
     .then(res=>res.json())
     .then(treedata=>displaytreedata(treedata.plants))
+    .catch(error => console.error('Error:', error)) 
 }
 
 const displaytreedata =(alltreedata)=>{
  const alltreecontent=document.querySelector(`#alltreecontent`);
+ alltreecontent.innerHTML=``;
  
   for( let alldata of alltreedata){
     const div= document.createElement(`div`);
     div.classList.add(`bg-white`,`px-3`,`pt-4`,`shadow-xl`,`rounded-lg`,`pb-3`);
     div.innerHTML=`<div class="w-[100%] h-[186px]  bg-gray-400">
-                     <img class="w-[100%] h-[100%] object-cover" src="${alldata.image}" alt="">
+                     <img  class="w-[100%] h-[100%] object-cover" src="${alldata.image}" alt="">
                     </div>
-                    <div>  <h2 class="font-semibold mt-2 text-xl">${alldata.name}</h2>
+                    <div>  <h2 onclick="showl(${alldata.id})"class="font-semibold mt-2 cursor-pointer text-xl">${alldata.name}</h2>
                         <p class="text-gray-500 my-3">${alldata.description}</p>
                         </div>
                        <div class="flex justify-between my-6">
@@ -55,10 +66,10 @@ const displaytreedata =(alltreedata)=>{
                        
                        
     alltreecontent.appendChild(div);
-    spinner(false);
+    
     
   }
-  
+  spinner(false);
     
 
 }
@@ -78,8 +89,9 @@ const subcetagory = (valueofid)=>{
         });
         let colorbtn= document.querySelector(`#coloranimate${valueofid}`);
         colorbtn.classList.add(`bg-[#15803d]`)
-        console.log(colorbtn)
+        
         displayeachcatvalue(dataceta.plants)})
+        .catch(error => console.error('Error:', error)) 
 
 }
 
@@ -89,6 +101,7 @@ const displayeachcatvalue =(mainvalue)=>{
 
     for(let eachvalue of mainvalue){
         const div= document.createElement(`div`);
+        console.log(eachvalue);
     div.classList.add(`bg-white`,`px-3`,`pt-4`,`shadow-xl`,`rounded-lg`,`pb-3`,`h-[55vh]`);
     div.innerHTML=`<div class="w-[100%] h-[186px]  bg-gray-400">
                      <img class="w-[100%] h-[100%] object-cover" src="${eachvalue.image}" alt="">
@@ -100,55 +113,96 @@ const displayeachcatvalue =(mainvalue)=>{
                         <h4 class="  px-2.5 py-1 rounded-3xl bg-[#dcfce7] text-green-600 ">${eachvalue.category}</h4>
                        <p class="text-xl font-semibold">${eachvalue.price}</p>
                         </div>
-                        <button class=" text-white text-xl py-4 rounded-full bg-[#15803d] w-full">Add to Cart</button>`
+                        <button onclick="pricefunc(${eachvalue.id})" class="  text-white  text-xl py-4 rounded-full bg-[#15803d] w-full">Add to Cart</button>`
     alltreecontent.appendChild(div);
-    spinner(false);
+    
+    
     
 
     }
+    spinner(false);
 
 }
 const pricefunc= (pricedataid)=>{
     fetch(`https://openapi.programming-hero.com/api/plant/${pricedataid}`)
     .then(res=>res.json())
     .then(alldetail=>pricemianfunc(alldetail.plants))
+    .catch(error => console.error('Error:', error)) 
+    
 
 }
-let count =0;
+
 let totalprice=0;
 const pricemianfunc=(maindetail1)=>{
     
-
-    const pricedib= document.querySelector(`#pricedib`);
+     const pricedib= document.querySelector(`#pricedib`);
 
     let div3= document.createElement(`div`);
-    div3.classList.add(`flex`, `bg-[#dcfce7]`,`items-center`,`py-3`,`px-2`,`rounded-xl`,`justify-between`, `w-full`,`mt-3`)
+    div3.classList.add(`flex`,`clear`, `bg-[#dcfce7]`,`items-center`,`py-3`,`px-2`,`rounded-xl`,`justify-between`, `w-full`,`mt-3`)
     div3.innerHTML=`<div>
                     <h1 class="text-xl font-semibold">${maindetail1.name}</h1>
                       <p class="price" class="text-gray-500">${maindetail1.price}</p>
                     </div>
                     <div>
-                        <h3><i onclick="clear()" class="fa-regular fa-circle-xmark text-2xl "></i></h3>
+                        <h3><button onclick="removeItem(this, ${maindetail1.price})" class="text-2xl cursor-pointer text-red-500 ">x</button></h3>
                     </div>`
 pricedib.appendChild(div3)
 
  
 
- let price=maindetail1.price;
+ let price=Number(maindetail1.price);
  totalprice +=price
 
   let totalfiv=document.querySelector(`#totalfiv`);
-  totalfiv.textContent=totalprice
+  totalfiv.textContent=totalprice;
 
 
-const clear=()=>{
-    div3
 
+
+
+}
+
+function removeItem(element, price) {
+    let cartItem = element.closest(".clear");
+   cartItem.remove();
+   totalprice= totalprice - Number(price);
+   let totalfiv=document.querySelector(`#totalfiv`);
+  totalfiv.textContent=totalprice;
+}
+
+
+const showl=(hello)=>{
+    fetch(`https://openapi.programming-hero.com/api/plant/${hello}`)
+    .then(res=>res.json())
+    .then(modal=>modaldisplay(modal.plants))
+    .catch(error => console.error('Error:', error)) 
     
 
 }
 
+const modaldisplay=(modalvalue)=>{
+     const mymodal= document.querySelector(`.modal-box`)
+    mymodal.innerHTML=`<div class="w-[100%] h-[286px]  ">
+                     <img class="w-[100%] h-[100%] object-cover" src="${modalvalue.image}" alt="">
+                    </div>
+                     <div>  <h2 class="font-semibold mt-2 text-xl">${modalvalue.name}</h2>
+                        <p class="text-gray-500 my-3">${modalvalue.description}</p>
+                        </div>
+                       <div class="flex justify-between my-6">
+                        <h4 class="  px-2.5 py-1 rounded-3xl bg-[#dcfce7] text-green-600 ">${modalvalue.category}</h4>
+                       <p class="text-xl font-semibold">${modalvalue.price}</p>
+                        </div>
+                       <div class=""> <button  class=" text-white text-xl py-4 rounded-full bg-[#15803d]  w-full">Add to Cart</button> </div>
+                       <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+                       `
 
+                       const mymodal5= document.querySelector(`#my_modal_5`)
+                       mymodal5.showModal()
+                    
 
 }
-
